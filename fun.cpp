a62bf1bbf17bc960886d3a0a71892d20541ec585
ttt <rupakrokade@gmail.com>
2019-09-01 14:21:10 +0530

    octave now accepts vectors and optional string args as input

--- fun.cpp
+++ fun.cpp
@@ -11,7 +11,7 @@
 extern "C"
 {
 
-int fun (double* answ, double* size, std::string name)
+int fun (double* answ, double* in1, int in1_row, std::string name, std::string opt)
 {
   //double answ;
 // Create interpreter.
@@ -38,6 +38,25 @@ int fun (double* answ, double* size, std::string name)
 		//octave_idx_type n = size;
 		octave_value_list in;
 
+	//std::vector<double> x;
+		Matrix inMatrix_x(in1_row, 1);
+		for( unsigned int i = 0; i < in1_row; i++ )
+		{
+				inMatrix_x(i, 0) = in1[i]; // inMatrix_x(i) would also do the job
+		}
+/*
+		std::cout << sizeof inMatrix_x / sizeof inMatrix_x(0,0) << "\n";
+		std::cout << "-------rows-------" << "\n";
+
+		std::cout << sizeof inMatrix_x(0,0) / sizeof(int) << "\n";
+		std::cout << "-------cols-------" << "\n";
+*/
+
+		in(0) = inMatrix_x;
+		Matrix mIn(in(0).matrix_value());
+		std::cout << mIn << "\n";
+
+/*
 		in(0) = octave_value(size);
 		//std::cout << in.length() << "\n";
 		//std::cout << in << "\n";
@@ -45,6 +64,10 @@ int fun (double* answ, double* size, std::string name)
 		for (octave_idx_type i = 0; i < in.length(); i++)
         in(i) = octave_value (size[i]);
 		
+*/
+		if(opt!="")
+			in(1) = opt;
+	
 
 		octave_value_list out = octave::feval (name, in, 1);
 		//octave_value_list out = octave::feval ("hamming", in, 1);
--- main.cpp
+++ main.cpp
@@ -4,15 +4,26 @@
 
 int main(void)
 {
+/*
   double* n;
-	double* val;// = 5.0;
-	val[0]=5.0;
+	double val = 5.0;
+	//val[0]=5.0;
 	n = &val;
 	double ar[(int)val];
 	//n[0]=3.0;
+*/
 
-	fun(ar, n, "hamming");
-//	for(int i=0; i<ar.length(); i++)
-		std::cout << ar[1] << std::endl;
+	int dim = 5;
+	double val = 15.0;
+	double in[dim];
+	for(int i=0; i<dim; i++)
+		in[i]=i+val;
+	
+	double ar[(int)val];
+	
+
+	fun(ar, in, dim, "sqrt","");
+	for(int i=0; i<dim; i++)
+		std::cout << ar[i] << std::endl;
 	return 0;
 }
