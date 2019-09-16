0b5738ab7317f737c893e099b7f2836f407b6495
rupak <rupakrokade@gmail.com>
2019-09-16 16:55:50 +0530

    Octave can now accept three input matrix

--- fun.cpp
+++ fun.cpp
@@ -31,36 +31,124 @@ extern "C"
 		
 		try
 		{	
+			unsigned int k=0;
+////////////////First Input////////////////
 			octave_value_list in;
-
-			Matrix inMatrix_x(inp->size_input1[2], 1);
-			
-			for( unsigned int i = 0; i < inp->size_input1[2]; i++ )
+			if(inp->size_input1[1]!=0 || inp->size_input1[2]!=0)
 			{
-					inMatrix_x(i, 0) = inp->input1[i]; 
-			}
 
-			in(0) = inMatrix_x;
-			Matrix mIn(in(0).matrix_value());
-			std::cout << mIn << "\n";
-		
+				Matrix inMatrix_x(inp->size_input1[1],inp->size_input1[2]);
+
+				for( unsigned int i = 0; i < inp->size_input1[1]; i++ )
+				{
+					for( unsigned int j = 0; j < inp->size_input1[2]; j++ )
+					{
+							inMatrix_x(i, j) = inp->input1[k]; 
+							k++;
+					}
+				}
+
+				in(0) = inMatrix_x;
+
+				Matrix mIn(in(0).matrix_value());
+				std::cout << "-------Input Matrix-------" << "\n";
+				std::cout << mIn << "\n";
+				std::cout << "--------------------" << "\n";
+			}
+////////////////Second Input////////////////
+			if(inp->size_input2[1]!=0 || inp->size_input2[2]!=0)
+			{
+				octave_value_list in2;
+
+				Matrix inMatrix_x2(inp->size_input2[1],inp->size_input2[2]);
+				k=0;
+				for( unsigned int i = 0; i < inp->size_input2[1]; i++ )
+				{
+					for( unsigned int j = 0; j < inp->size_input2[2]; j++ )
+					{
+							inMatrix_x2(i, j) = inp->input2[k]; 
+							k++;
+					}
+				}
+
+				in(1) = inMatrix_x2;
+				Matrix mIn2(in(1).matrix_value());
+				std::cout << "-------Input Matrix-------" << "\n";
+				std::cout << mIn2 << "\n";
+				std::cout << "--------------------" << "\n";
+
+			}
+////////////////Second Input////////////////
+			if(inp->size_input3[1]!=0 || inp->size_input3[2]!=0)
+			{
+				octave_value_list in3;
+
+				Matrix inMatrix_x3(inp->size_input3[1],inp->size_input3[2]);
+				k=0;
+				for( unsigned int i = 0; i < inp->size_input3[1]; i++ )
+				{
+					for( unsigned int j = 0; j < inp->size_input3[2]; j++ )
+					{
+							inMatrix_x3(i, j) = inp->input3[k]; 
+							k++;
+					}
+				}
+
+				in(2) = inMatrix_x3;
+				Matrix mIn3(in(2).matrix_value());
+				std::cout << "-------Input Matrix-------" << "\n";
+				std::cout << mIn3 << "\n";
+				std::cout << "--------------------" << "\n";
+
+
+			}
+//-------------------------------------------------------
 			if(inp->name2)
 			{
-				in(1) = inp->name2;
+				in(3) = inp->name2;
 			}
 
+			octave::feval ("pkg", ovl("load", "signal"), 0);
+/*
+			if(inp->package)
+			{
+				std::cout << "Loading package: " << inp->package << "\n";
+				octave::feval ("pkg", ovl("load", inp->package), 0);
+			}
+*/
 			octave_value_list out = octave::feval (inp->name1, in, 1);
 
 			Matrix mOut(out(0).matrix_value());
-			//std::cout << mOut << "\n";
-			int len = mOut.numel();
-			inp->size_output1[1] = len;
-			inp->output1 = new double[len];
 
-			for(int i=0; i<len; i++)
-			{
-				inp->output1[i] = mOut(i);
-			}	
+
+			//std::cout << mOut << "\n";
+
+
+
+			
+			int row1 = mOut.rows();
+			int col1 = mOut.columns();
+			//std::cout << "--output matrix: " << row1 << "X" << col1 << "\n";
+
+			inp->size_output1[1] = row1;
+			inp->size_output1[2] = col1;
+			int len = row1*col1;
+			inp->output1 = new double[len];
+			k=0;
+
+			for(unsigned int i=0;i<row1;i++)
+				{
+					for(unsigned int j=0;j<col1;j++)
+					{
+						inp->output1[k]=mOut(k);
+						k++;
+					}
+				}
+
+//			for(int i=0; i<len; i++)
+//			{
+//				inp->output1[i] = mOut(i);
+//			}	
 		}
 		catch (const octave::exit_exception& ex)
 		{
--- main.cpp
+++ main.cpp
@@ -10,20 +10,145 @@ int main(void)
 
 	octf *inptr = &ins;
 
-	ins.name1 = "sqrt";
+	ins.name1 = "arch_rnd";
+	ins.package = "signal";
 	ins.name2 = NULL; //Passing "" will not work
 
+//////////////First Input///////////////////
+	int row1 = 1;
+	int col1 = 5;
+	int i,j,k=0;
+	int imat[row1][col1];
+	int dim = row1*col1;
+std::cout << "--Input matrix1: " << row1 << "X" << col1 << "\n";
+
+/*		
+	for(i=0;i<row1;i++)
+	{
+		for(j=0;j<col1;j++)
+		{
+			imat[i][j]=i+j+5;
+			printf("%d ",imat[i][j]);
+		}
+	printf("\n");
+	}
+*/
 
 
-	int dim = 1;
 	ins.input1 = new double[dim];
-	ins.size_input1[2]=dim;
+	ins.size_input1[1]=row1;
+	ins.size_input1[2]=col1;
+/*
+	for(i=0;i<row1;i++)
+	{
+		for(j=0;j<col1;j++)
+		{
+			ins.input1[k]=imat[i][j];
+			k++;
+		}
+	}
+*/
 
-	double val = 5.0;
+	ins.input1[0] = 1;
+  ins.input1[1] = 2;
+  ins.input1[2] = 3;
+  ins.input1[3] = 4;
+  ins.input1[4] = 5;
 
-	for(int i=0; i<dim; i++)
-		ins.input1[i]=i+val;
-	
+
+
+//////////////Second Input///////////////////
+	int row2 = 1;
+	int col2 = 4;
+	//i,j,k=0;
+	int imat2[row2][col2];
+	int dim2 = row2*col2;
+std::cout << "--Input matrix2: " << row2 << "X" << col2 << "\n";
+
+/*		
+	for(i=0;i<row1;i++)
+	{
+		for(j=0;j<col1;j++)
+		{
+			imat[i][j]=i+j+5;
+			printf("%d ",imat[i][j]);
+		}
+	printf("\n");
+	}
+*/
+
+
+	ins.input2 = new double[dim2];
+	ins.size_input2[1]=row2;
+	ins.size_input2[2]=col2;
+/*
+	for(i=0;i<row1;i++)
+	{
+		for(j=0;j<col1;j++)
+		{
+			ins.input1[k]=imat[i][j];
+			k++;
+		}
+	}
+*/
+
+	ins.input2[0] = 7;
+  ins.input2[1] = 8;
+  ins.input2[2] = 9;
+  ins.input2[3] = 10;
+//  ins.input2[4] = 5;
+
+//	for(i=0;i<dim;i++)
+//		printf("%d ",ins.input1[i]);
+
+//	printf("\n");
+
+//////////////Second Input///////////////////
+	int row3 = 1;
+	int col3 = 1;
+	//i,j,k=0;
+	int imat3[row2][col2];
+	int dim3 = row3*col3;
+std::cout << "--Input matrix3: " << row3 << "X" << col3 << "\n";
+
+/*		
+	for(i=0;i<row1;i++)
+	{
+		for(j=0;j<col1;j++)
+		{
+			imat[i][j]=i+j+5;
+			printf("%d ",imat[i][j]);
+		}
+	printf("\n");
+	}
+*/
+
+
+	ins.input3 = new double[dim3];
+	ins.size_input3[1]=row3;
+	ins.size_input3[2]=col3;
+/*
+	for(i=0;i<row1;i++)
+	{
+		for(j=0;j<col1;j++)
+		{
+			ins.input1[k]=imat[i][j];
+			k++;
+		}
+	}
+*/
+
+	ins.input3[0] = 5;
+//  ins.input3[1] = 1;
+//  ins.input3[2] = 3;
+ // ins.input3[3] = 4;
+//  ins.input3[4] = 5;
+
+//	for(i=0;i<dim;i++)
+//		printf("%d ",ins.input1[i]);
+
+//	printf("\n");
+//----------------------------------------
 
 	int status_fun = fun(inptr);
 
@@ -34,13 +159,31 @@ int main(void)
 	}
 	else
 	{
-		for(int i=0; i<ins.size_output1[1]; i++)
+//		for(int i=0; i<ins.size_output1[1]; i++)
+//		{
+//			std::cout<< i+1 <<"\t" << ins.output1[i] << std::endl;
+//		}
+
+	int out_dim = ins.size_output1[1] * ins.size_output1[2];
+	k=0;
+
+	std::cout << "--output matrix: " << ins.size_output1[1] << "X" << ins.size_output1[2] << "\n";
+	for(i=0;i<ins.size_output1[1];i++)
+	{
+		for(j=0;j<ins.size_output1[2];j++)
 		{
-			std::cout<< i+1 <<"\t" << ins.output1[i] << std::endl;
+			std::cout << ins.output1[k] << " ";
+			k++;
 		}
+	std::cout << '\n';
+	}
+
+std::cout << '\n';
 
 		free(ins.output1);
 		free(ins.input1);
+		free(ins.input2);
+		free(ins.input3);
 	}
 	return 0;
 }
